869c56d43c6b5a75218f54237377abdc6ce4793b
yjjungyo <yjjungyo@gmail.com>
2021-04-08 18:11:43 +0900

    init

---
+++ src/main/java/com/factory/happickliveproduct/HappickliveProductApplication.java
@@ -0,0 +1,13 @@
+package com.factory.happickliveproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappickliveProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappickliveProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factory/happickliveproduct/HappickliveProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factory.happickliveproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappickliveProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
